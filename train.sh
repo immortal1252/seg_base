ec168109bc4a308f65d675fa13d1799d3f881e85
spg <2756697676@qq.com>
2024-03-15 16:28:44 +0800

    fixmatch第一次实现

--- train.sh
+++ train.sh
@@ -1,5 +1,1 @@
-source activate anno
-CUDA_VISIBLE_DEVICES=1 \
-nohup python train.py --config "./configs/unet_base_20.yaml">unet_base_20.out 2>&1 &
-CUDA_VISIBLE_DEVICES=2 \
-nohup python train.py --config "./configs/unet_base_leakyrelu20_2.yaml">unet_base_leakyrelu20_2.out 2>&1 &
+nohup python fixmatch.py --config "./config/eunet_fixmatch.yaml">eunet_fixmatch.out 2>&1 &
