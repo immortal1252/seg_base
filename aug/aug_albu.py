24d14c8ba01b445fce16eb4c0c2436d85ac7aa62
zcr <2756697676@qq.com>
2024-05-08 17:17:37 +0800

    增加FixedLengthSampler保证supportset和targetset一样大

--- aug/aug_albu.py
+++ aug/aug_albu.py
@@ -7,10 +7,11 @@ from albumentations.pytorch import ToTensorV2
 
 # # BUSI
 MEAN, STD = 0.32963505387306213, 0.2224094420671463
+H = 384
 # # dataset_b
 # # MEAN, STD = 0.24798217310678738, 0.15146150910781206
 norm = A.Normalize((MEAN, MEAN, MEAN), (STD, STD, STD))
-resize = A.Resize(448, 448)
+resize = A.Resize(H, H)
 totensor = ToTensorV2()
 norm_totensor = A.Compose([norm, totensor])
 
@@ -24,7 +25,8 @@ def mask2tensor(mask):
 
 basic_aug = A.Compose(
     [
-        A.RandomResizedCrop(448, 448, scale=(0.5, 1)),
+        A.RandomResizedCrop(H, H, scale=(0.5, 1)),
+        # A.Resize(H, H),
         A.HorizontalFlip(p=0.5),
     ]
 )
--- universeg.sh
+++ universeg.sh
@@ -1,1 +1,6 @@
-nohup python -u universeg_pipeline.py --path config/universeg.yaml > universeg.out &
+# python -u universeg_pipeline.py --path config/universeg4.yaml
+# python -u universeg_pipeline.py --path config/universeg5.yaml
+# python -u universeg_pipeline.py --path config/universeg6.yaml
+# python -u universeg_pipeline.py --path config/universeg7.yaml
+
+nohup python -u universeg_pipeline.py --path  universeg_config/universeg_big.yaml > universeg_base.out &
