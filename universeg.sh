94c5072220e15cfb7cbe2aa291efb12e7cb8ec6d
zcr <2756697676@qq.com>
2024-05-08 19:20:58 +0800

    测试更大的模型

--- universeg.sh
+++ universeg.sh
@@ -1,6 +1,3 @@
-# python -u universeg_pipeline.py --path config/universeg4.yaml
-# python -u universeg_pipeline.py --path config/universeg5.yaml
-# python -u universeg_pipeline.py --path config/universeg6.yaml
-# python -u universeg_pipeline.py --path config/universeg7.yaml
-
-nohup python -u universeg_pipeline.py --path  universeg_config/universeg_big.yaml > universeg_base.out &
+python -u universeg_pipeline.py --path universeg_config/universeg_base2_big_batch.yaml
+python -u universeg_pipeline.py --path universeg_config/universeg_base2_big_batch_lr-.yaml
+python -u universeg_pipeline.py --path universeg_config/universeg_base2_big_batch_lr+.yaml
