2ed7fa72ae37bbda46fc573f8e8b0bd0dbc9b910
spg <2756697676@qq.com>
2024-05-20 20:36:30 +0800

    block的激活函数优先使用inplace=True

--- unet/blocks.py
+++ unet/blocks.py
@@ -4,7 +4,7 @@ import torch
 
 class ConvBNAct(nn.Sequential):
     def __init__(
-        self, in_channels, out_channels, kernel_size, dilation=1, stride=1, act="ReLU"
+            self, in_channels, out_channels, kernel_size, dilation=1, stride=1, act="ReLU"
     ):
         super().__init__()
         padding = dilation * (kernel_size - 1) // 2
@@ -30,5 +30,10 @@ class Upsample(nn.Sequential):
 
 def get_act(act: str):
     if hasattr(nn, act):
-        return getattr(nn, act)()
+        activation_class = getattr(nn, act)
+        if 'inplace' in activation_class.__init__.__code__.co_varnames:
+            instance = activation_class(inplace=True)
+        else:
+            instance = activation_class()
+        return instance
     return nn.Identity()
