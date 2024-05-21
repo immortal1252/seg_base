9cf29643d5a4e5f10760e511b3b46cafce496b1f
zcr <2756697676@qq.com>
2024-05-21 12:13:58 +0800

    大模型论剑，res_single_se最好，aaunet次之

--- universeg.sh
+++ universeg.sh
@@ -1,2 +1,5 @@
-python -u universeg_pipeline.py --path full/res_stack_se2.yaml
-# shutdown -h now
+python -u universeg_pipeline.py --path full/bigger/res_stack_se.yaml
+python -u supervised.py --path full/bigger/aaunet.yaml
+python -u supervised.py --path full/bigger/attnunet.yaml
+python -u supervised.py --path full/bigger/unet.yaml
+shutdown -h now
