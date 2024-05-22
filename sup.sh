e5b2cacfa1d0799d51778bb275406ec7408af4e8
zcr <2756697676@qq.com>
2024-05-22 22:52:26 +0800

    transunet跑出来了

--- sup.sh
+++ sup.sh
@@ -1,3 +1,3 @@
-python supervised.py --path  unet_config/attnunet.yaml > sup.out
+python supervised.py --path unet_config/transunet.yaml > sup.out
 # python supervised.py --path  unet_config/skunet.yaml > sup.out
 # shutdown -h now
