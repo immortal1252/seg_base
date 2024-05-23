da6a27975dfa26c40984ba9847d85c010f451bb1
zcr <2756697676@qq.com>
2024-05-23 15:45:44 +0800

    分别打印良性和恶性的准确率

---
+++ universeg_cla.sh
@@ -0,0 +1,2 @@
+python -u universeg_cla_pipeline.py --path full/res_stack_se/leaky_relu_cla.yaml
+shutdown
--- universeg_cla_pipeline.py
+++ universeg_cla_pipeline.py
@@ -1,111 +1,12 @@
 import argparse
 
 import torch
-from torch.utils.data import DataLoader
 import torch.utils.data.sampler
-import random
-import spgutils.pipeline
-from typing import Sized
+
+import universeg_pipeline
 
 
-class FixedLengthSampler(torch.utils.data.sampler.Sampler):
-    """
-    固定采样个数，随机打乱后循环采样
-    """
-
-    def __init__(self, dataset, num_samples):
-        super().__init__(None)
-        self.dataset = dataset
-        self.num_samples = num_samples
-
-    def __iter__(self):
-        indices = list(range(len(self.dataset)))
-        random.shuffle(indices)
-        cnt = 0
-        while cnt < self.num_samples:
-            yield indices[cnt % len(self.dataset)]
-            cnt += 1
-
-    def __len__(self):
-        return self.num_samples
-
-
-class UniversegPipeline(spgutils.pipeline.Pipeline):
-    def prepare_data(self):
-        batch_size = self.config["batch_size"]
-        k = int(self.config["k"])
-        train_loader = DataLoader(self.trainset, batch_size=batch_size, shuffle=True)
-        support_loader_list_train = [
-            DataLoader(self.trainset, batch_size=batch_size, shuffle=True)
-            for _ in range(k)
-        ]
-
-        test_k = int(self.config.get("test_k", k))
-        assert isinstance(self.testset, Sized)  # escape warning
-        support_loader_list_test = [
-            DataLoader(
-                self.trainset,
-                sampler=FixedLengthSampler(self.trainset, len(self.testset)),
-                batch_size=batch_size,
-            )
-            for _ in range(test_k)
-        ]
-
-        test_loader = DataLoader(self.testset, batch_size=batch_size, shuffle=False)
-
-        if hasattr(self, "validset"):
-            valid_loader = DataLoader(
-                self.validset, batch_size=batch_size, shuffle=False
-            )
-            assert isinstance(self.validset, Sized)  # escape warning
-            support_loader_list_valid = [
-                DataLoader(
-                    self.trainset,
-                    sampler=FixedLengthSampler(self.trainset, len(self.validset)),
-                    batch_size=batch_size,
-                )
-                for _ in range(test_k)
-            ]
-        else:
-            valid_loader = None
-            support_loader_list_valid = None
-        return (
-            (train_loader, support_loader_list_train),
-            (
-                test_loader,
-                support_loader_list_test,
-            ),
-            (valid_loader, support_loader_list_valid),
-        )
-
-    def train_epoch(self, train_support_loader):
-        train_loader, support_loader_list = train_support_loader
-        support_loader = zip(*support_loader_list)
-        self.model.train()
-        epoch_loss = 0
-        for (u, uy, *_), (v_vy_tuples) in zip(train_loader, support_loader):
-            # kv:(v1,vy2),(v2,vy2)...(vk,vyk)
-            u = u.to(self.device)
-            uy = uy.to(self.device)
-
-            vs = [v for v, vy, *_ in v_vy_tuples]
-            vys = [vy for v, vy, *_ in v_vy_tuples]
-            vs = torch.stack(vs, dim=1)
-            vys = torch.stack(vys, dim=1)
-            vs = vs.to(self.device)
-            vys = vys.to(self.device)
-
-            try:
-                logits = self.model(u, vs, vys)
-            except Exception as e:
-                print(e)
-                pass
-            loss = self.criterion(logits, uy)
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-            epoch_loss += loss.item()
-        return epoch_loss
+class UniversegClaPipeline(universeg_pipeline.UniversegPipeline):
 
     def evaluate(self, test_support_loader):
         test_loader, support_loader_list = test_support_loader
@@ -116,7 +17,7 @@ class UniversegPipeline(spgutils.pipeline.Pipeline):
         intersection = [0, 0]
         union = [0, 0]
         for batch_id, ((u, uy, cla), (v_vy_tuples)) in enumerate(
-                zip(test_loader, support_loader)
+            zip(test_loader, support_loader)
         ):
             u = u.to(self.device)
             uy = uy.to(self.device)
@@ -134,10 +35,13 @@ class UniversegPipeline(spgutils.pipeline.Pipeline):
             y_pred = torch.where(y_pred > 0, 1, 0)
             self.save_y_ypred(uy, y_pred, batch_id)
 
-            intersection[cla] += torch.sum(uy * y_pred).item()
-            union[cla] += torch.sum(y_pred + uy).item()
-            tp[cla] += torch.sum(y_pred == uy).item()
-            pixel_cnt[cla] += uy.numel()
+            for i in range(2):
+                idx = torch.nonzero(cla == i).flatten()
+                intersection[i] += torch.sum(uy[idx] * y_pred[idx]).item()
+                union[i] += torch.sum(y_pred[idx] + uy[idx]).item()
+                tp[i] += torch.sum(y_pred[idx] == uy[idx]).item()
+                pixel_cnt[i] += uy[idx].numel()
+
         for i in range(2):
             dice_avg_t = (2 * intersection[i] + 1e-9) / (union[i] + 1e-9)
             acc_avg_t = tp[i] / pixel_cnt[i]
@@ -153,5 +57,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", type=str, required=True)
     args = parser.parse_args()
-    supervised = UniversegPipeline(args.path)
+    supervised = UniversegClaPipeline(args.path)
     supervised.train()
